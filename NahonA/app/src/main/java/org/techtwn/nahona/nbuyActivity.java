26f50ef87f3240833ee6f1eb6a07746be6d6fc7c
hyukin <49222157+hyukin@users.noreply.github.com>
2020-11-11 00:22:21 +0900

    파이어베이스 연동
    파이어베이스 연동

--- NahonA/app/src/main/java/org/techtwn/nahona/nbuyActivity.java
+++ NahonA/app/src/main/java/org/techtwn/nahona/nbuyActivity.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class nbuyActivity extends Fragment {
+    TextView tv;
+    EditText et;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        tv =
         return inflater.inflate(R.layout.activity_nbuy,container,false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
