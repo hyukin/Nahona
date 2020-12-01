7b03db4b0518959a320cd8a0a9715e27f5d92e00
20173159choi <73225977+20173159choi@users.noreply.github.com>
2020-12-01 20:04:44 +0900

    로그인
    로그인

--- NahonA/app/src/main/java/org/techtwn/nahona/loginActivity.java
+++ NahonA/app/src/main/java/org/techtwn/nahona/loginActivity.java
@@ -4,31 +4,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextInputLayout passwordLayout = findViewById(R.id.input_password);
+        firebaseAuth =  FirebaseAuth.getInstance();
 
-        passwordLayout.setPasswordVisibilityToggleEnabled(true);
+        mEmailText = findViewById(R.id.input_email);
+        mPasswordText = findViewById(R.id.input_password);
 
         Button button = (Button)findViewById(R.id.login_button);
         TextView signup = (TextView)findViewById(R.id.signupText);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-                startActivityForResult(intent,1001);
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(loginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(loginActivity.this, homeActivity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(loginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
 
